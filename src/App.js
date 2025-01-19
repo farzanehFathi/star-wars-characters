ef5622d6c61788e031016e427e99bff9836e13ad
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2025-01-19 13:22:43 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
           />
         </Route>
 
-        <Route path="films" element={<Films />} />
+        {/* <Route path="films" element={<Films />} /> */}
         <Route path="*" element={<NotFound />} />
       </Route>
     )
