e0b63b3996153fcd5713e5ad46bace6624e10b8c
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2025-01-19 13:43:13 +0330

    NotFound finalized

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,15 +1,17 @@
-import { Flex, Space, Typography } from "antd";
+import { Flex, Typography } from "antd";
 
 import UnderConstruction from "../components/UnderConstruction";
 
 const NotFound = () => {
   return (
-    <Flex justify="center">
-      <Typography.Paragraph style={{ fontSize: 24 }} type="warning">
-        SWAPI is Under Construction
-      </Typography.Paragraph>
+    <>
+      <Flex justify="center">
+        <Typography.Paragraph style={{ fontSize: 24 }} type="warning">
+          SWAPI is Under Construction
+        </Typography.Paragraph>
+      </Flex>
       <UnderConstruction />
-    </Flex>
+    </>
   );
 };
 
